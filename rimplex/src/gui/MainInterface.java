f22c92637b065fd96749576b4dadac4ad9bc8e4b
hendrijn <hendrijn@dukes.jmu.edu>
2021-03-23 15:39:28 -0400

    Main window for interface.

---
+++ rimplex/src/gui/MainInterface.java
@@ -0,0 +1,16 @@
+package gui;
+
+import javax.swing.JFrame;
+
+/**
+ * Serves as the main window for the interface.
+ *
+ * @author Jacquelyn Hendricks
+ * @version March 23 2021
+ */
+public class MainInterface extends JFrame
+{
+
+  private static final long serialVersionUID = 2006081507159558070L;
+  
+}
