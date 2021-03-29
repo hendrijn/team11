0e73bc11b04a3331233a10c5e80e2f7ad1a2773b
pgleb <glebuspm@dukes.jmu.edu>
2021-03-29 18:43:09 -0400

    Changelog:
    -Added two tests for Addition
    -Added error message method in mainInterface
    -Added error message handling for the equals button

--- rimplex/src/testing/AdditonOperatorTesting.java
+++ rimplex/src/testing/AdditonOperatorTesting.java
@@ -512,6 +512,35 @@ class AdditonOperatorTesting
   
   
   
+  @Test
+  public void singleNegMIRightTest()
+  {
+    String complexNumberOne = "2 + 3i";
+    String complexNumberTwo = "-6i";
+    String expectedResult = "2+-3i";
+    TempContext tempContext = new TempContext(new AdditionOperator());
+    String actualResult = tempContext.evaluate(complexNumberOne, complexNumberTwo);
+    assertEquals(expectedResult, actualResult);
+    
+  }
+  
+  
+
+  
+  
+  @Test
+  public void operandsWithSubtractionTest()
+  {
+    String complexNumberOne = "2 + 3i";
+    String complexNumberTwo = "2 - 2i";
+    String expectedResult = "4 + -1i";
+    TempContext tempContext = new TempContext(new AdditionOperator());
+    String actualResult = tempContext.evaluate(complexNumberOne, complexNumberTwo);
+    assertEquals(expectedResult, actualResult);
+ 
+  }
+  
+  
   
   
   
