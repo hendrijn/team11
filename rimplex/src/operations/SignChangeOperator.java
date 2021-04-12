4c1173dcbca79a16286940a1df3f8b077a618de3
sydneymay2001 <sydneymay2001@10.252.13.16>
2021-04-12 13:04:49 -0400

    finished sign change class (except for random string handling).
    added error checking for illegal args to sign change tests.

--- rimplex/src/operations/SignChangeOperator.java
+++ rimplex/src/operations/SignChangeOperator.java
@@ -4,18 +4,73 @@ public class SignChangeOperator
 {
   public String changeSign(String operand) throws IllegalArgumentException
   {
-    //TODO: handle illegal args
-    ConjugateOperator c = new ConjugateOperator();
-    String conjugatedOp = c.conjugate(operand);
-    String result = "";
-    
-    if(conjugatedOp.charAt(0) == '-')
+    // TODO: handle random strings
+    //error checking 
+    if (operand == null)
     {
-      result = conjugatedOp.substring(1);
+      throw new IllegalArgumentException("Please provide an operand");
+    }
+    
+    String alteredOp = operand.replaceAll(" ", "");
+    
+    long iCount  = alteredOp.chars().filter(ch -> ch == 'i').count();
+    if (iCount > 1 || alteredOp.isEmpty() || alteredOp.equals("()"))
+    {
+      throw new IllegalArgumentException("Please provide a valid operand, or simplify it.");
+    }
+
+    
+    boolean complex = false;
+    boolean imaginary = false;
+    boolean real = false;
+    String result = "";
+
+    if (alteredOp.contains("+"))
+    {
+      complex = true;
+    }
+    else if (alteredOp.charAt(0) == '-' && alteredOp.indexOf("-", 1) != -1)
+    {
+      complex = true;
+    }
+    else if (alteredOp.charAt(0) != '-' && alteredOp.contains("-"))
+    {
+      complex = true;
+    }
+    else if (alteredOp.contains("i"))
+    {
+      imaginary = true;
     }
     else
     {
-      result = "-" + conjugatedOp;
+      real = true;
+    }
+
+    if (complex)
+    {
+      ConjugateOperator c = new ConjugateOperator();
+      String conjugatedOp = c.conjugate(operand);
+      if (conjugatedOp.charAt(0) == '-')
+      {
+        result = conjugatedOp.substring(1);
+      }
+      else
+      {
+        result = "-" + conjugatedOp;
+      }
+    } 
+    
+    if(imaginary || real)
+    {
+      if(alteredOp.charAt(0) == '-')
+      {
+        result = alteredOp.substring(1);
+      } 
+      else
+      {
+        result = "-" + alteredOp;
+      }
+
     }
     return result;
   }
