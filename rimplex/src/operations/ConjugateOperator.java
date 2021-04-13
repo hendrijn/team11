82e9c04c667655f4ae4fe09a8cbeb43b0116b3b6
sydneymay2001 <sydneymay2001@10.252.13.16>
2021-04-13 11:15:12 -0400

    Fixed conjugate to conjugate imaginary numbers.

--- rimplex/src/operations/ConjugateOperator.java
+++ rimplex/src/operations/ConjugateOperator.java
@@ -1,41 +1,60 @@
 package operations;
 
-public class ConjugateOperator 
+public class ConjugateOperator
 {
 
   public String conjugate(String operand)
   {
-    //TODO: handle illegal args
-    
-    if(operand == null || operand.equals(""))
+    // TODO: handle illegal args
+
+    if (operand == null || operand.equals(""))
     {
       throw new IllegalArgumentException("Please Enter an Operand.");
     }
-    
+
     String alteredOp = ((operand.replaceAll(" ", "")).replace("(", "")).replace(")", "");
-    
-    if(alteredOp.equals(""))
+
+    if (alteredOp.equals(""))
     {
       throw new IllegalArgumentException("Please Enter an Operand.");
     }
-    
+
     String result = "";
-    int negMin = alteredOp.indexOf("-");
-    int min = alteredOp.indexOf("-", negMin + 1);
-    
-    if(alteredOp.contains("+"))
+
+    boolean complex = TempContext.isComplex(alteredOp);
+    boolean imaginary = TempContext.isImaginary(alteredOp);
+
+    if (complex)
     {
-      result = alteredOp.substring(0, alteredOp.indexOf("+")) + "-" + alteredOp.substring(alteredOp.indexOf("+") + 1);
-    } 
-    else 
-    {
-      if(min != -1)
+      int negMin = alteredOp.indexOf("-");
+      int min = alteredOp.indexOf("-", negMin + 1);
+
+      if (alteredOp.contains("+"))
       {
-        result = alteredOp.substring(0, min) + "+" + alteredOp.substring(min + 1);
+        result = alteredOp.substring(0, alteredOp.indexOf("+")) + "-"
+            + alteredOp.substring(alteredOp.indexOf("+") + 1);
       }
       else
       {
-        result = alteredOp.substring(0, negMin) + "+" + alteredOp.substring(negMin + 1);
+        if (min != -1)
+        {
+          result = alteredOp.substring(0, min) + "+" + alteredOp.substring(min + 1);
+        }
+        else
+        {
+          result = alteredOp.substring(0, negMin) + "+" + alteredOp.substring(negMin + 1);
+        }
+      }
+    }
+    if(imaginary)
+    {
+      if(alteredOp.charAt(0) == '-')
+      {
+        result = alteredOp.substring(1);
+      }
+      else
+      {
+        result = "-" + alteredOp;
       }
     }
     return result;
